7b1201a64c35334e59becc4a9dc0770a46564b3d
Thomas Pengo <thomas.pengo@gmail.com>
2015-02-05 12:19:52 -0600

    First working version
    Changed Converter to class within a module. Added comments.  Added
    coments to VIPS_exe and new function to configure where to find the VIPS
    executable.

--- Converter.py
+++ Converter.py
@@ -1,40 +1,85 @@
 import tkFileDialog as tf
 from Tkinter import *
+import os, datetime
+from time import sleep
+import VISP_exe
 
-root = Tk()
+'''
+Converter : a simple file conversion dialog box to convert a bunch of files in a directory recursively to the Google Maps tiled format.
 
-Label(root, text = "Welcome! Select an input and output directory to start.").grid(row=0,column=0,columnspan=3)
+Just call
+	python Converter.py
 
-Label(root, text = "Input directory").grid(row=1,column=0)
-Label(root, text = "Output directory").grid(row=2,column=0)
+Author: Thomas Pengo
+License : GPL 3
+'''
 
-indir = ''
-outdir = ''
+class ConverterDialog(Frame):
+	indir = ''
+	outdir = ''
 
-indir_e = Entry(root, text=''); indir_e.grid(row=1,column=1)
-outdir_e = Entry(root, text=''); outdir_e.grid(row=2,column=1)
-
-def changeInDir():
-	indir = tf.askdirectory()
-	indir_e.delete(0)
-	indir_e.insert(0,indir)
+	def __init__(self, parent):
+		Frame.__init__(self, parent)
+		
+		self.parent = parent
+		
+		self.initUI()
 	
-def changeOutDir():
-	outdir = tf.askdirectory()
-	outdir_e.delete(0)
-	outdir_e.insert(0,outdir)
+	def initUI(self):
+		self.pack(fill=BOTH,expand=1)
+
+		Label(self, text = "Welcome! Select an input and output directory to start.").grid(row=0,column=0,columnspan=3)
+
+		Label(self, text = "Input directory").grid(row=1,column=0,sticky=E)
+		Label(self, text = "Output directory").grid(row=2,column=0,sticky=E)
+
+		self.indir_e = Entry(self, text=''); self.indir_e.grid(row=1,column=1,sticky=W+E)
+		self.outdir_e = Entry(self, text=''); self.outdir_e.grid(row=2,column=1,sticky=W+E)
+
+		self.indir_b = Button(self, text="Browse", command=self.changeInDir); self.indir_b.grid(row=1,column=2,sticky=W)
+		self.outdir_b = Button(self, text="Browse", command=self.changeOutDir); self.outdir_b.grid(row=2,column=2,sticky=W)
+		
+		self.logBox = Text(self, width=100, height=10); 
+		self.logBox.insert(END, str(datetime.datetime.today())); 
+		self.logBox.grid(row=3,column=0,columnspan=3,rowspan=4,sticky=W+E+N+S);
+
+		Button(self,text="Go!", command=self.go).grid(row=7,column=0,columnspan=3)
+
+	def changeInDir(self):
+		self.indir = tf.askdirectory()
+		self.indir_e.delete(0)
+		self.indir_e.insert(0,self.indir)
 	
-indir_b = Button(root, text="Browse", command=changeInDir); indir_b.grid(row=1,column=2)
-outdir_b = Button(root, text="Browse", command=changeOutDir); outdir_b.grid(row=2,column=2)
-
-log = Entry(root); log.insert(0, ">>"); log.grid(row=3,column=0,columnspan=3,rowspan=4,sticky=W+E+N+S);
-
-def go():
-	indir = indir_e.get()
-	outdir = outdir_e.get()
+	def changeOutDir(self):
+		self.outdir = tf.askdirectory()
+		self.outdir_e.delete(0)
+		self.outdir_e.insert(0,self.outdir)
 	
-	print indir, outdir
+	def log(self,text):
+		for line in text.split('\n'):
+			self.logBox.insert(END,"\n "+line)
+		
+		self.parent.update_idletasks()
+		
+		print text
 
-Button(root,text="Go!", command=go).grid(row=7,column=0,columnspan=3)
+	def doFile(self,input, output):
+		self.log('Starting '+input+' >> '+output)
+		
+		
 
-root.mainloop()
+	def go(self):
+		indir = self.indir_e.get()
+		outdir = self.outdir_e.get()
+
+		for root, dirs, files in os.walk(indir, topdown=True):
+			for infile in files:
+				infilepath = os.path.join(root,infile)
+				outfilepath = os.path.join(outdir,infile.rsplit('.')[0])
+				
+				self.doFile(infilepath, outfilepath)
+
+if __name__ == '__main__':
+	root = Tk()
+	app = ConverterDialog(root)
+	root.mainloop()
--- VIPS_exe.py
+++ VIPS_exe.py
@@ -1,8 +1,23 @@
 # File conversion utilities library
 import os, shutil
 
+'''
+Tiny wrapper around the VIPS executable.
+
+To use it simply:
+	import VIPS_exe
+	
+	VIPS_dzsave('path-to-input-file','path-to-output-dirName')
+
+Author: Thomas Pengo
+License: GPL 3
+'''
+
 VIPS_EXECUTABLE="C:\\vips-dev-7.42.0\\bin\\vips.exe "
 TEMPLATE_FILE=os.path.join(os.path.dirname(__file__),"template.html")
+
+def VIPS_setPath(newPath):
+	VIPS_EXECUTABLE=newPath
 
 def VIPS_call(command, inputFile, outputFile, parameters):
 	import subprocess
